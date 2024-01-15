0514d52316b95062891f1eee14394f7ead0f851b
melina ghassabi <149719824+Melinaghassabi@users.noreply.github.com>
2024-01-15 22:02:53 +0330

    homeworke7_1

---
+++ homeworke7_1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std; 
+  int main() { 
+ 
+  int m,n;
+   
+   int s;
+   
+    cout<<"please enter your number:"; 
+	cin>>m; 
+	cin>>n;
+	 for (s= 1= 0; n > 0; i--) 
+	  {  
+	       s=s*m;
+	    } 
+		cout<<s;
+		
+	}
+
